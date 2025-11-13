75584569c2c84a38d4ab8a325a527b255d447657
Cassie Huber <chuber@hawaii.edu>
2025-11-12 18:34:32 -1000

    Create meter_data.py

---
+++ meter_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import numpy as np
+
+def validate_base_path(path):
+    """
+    Validate if the base path exists.
+
+    Parameters:
+        path (str): The base path to validate.
+    
+    Returns:
+        boo
+    """
